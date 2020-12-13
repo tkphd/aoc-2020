ac7790673d7e8f829af6fc7b78459436551b08b4
Trevor Keller <trevor.keller@gmail.com>
2020-12-13 14:22:00 -0500

    valid solution to Day 6 part A

--- day-06/a.cpp
+++ day-06/a.cpp
@@ -6,7 +6,32 @@
 #include <string>
 #include <vector>
 
+const bool verbose = true;
+
 int main(int argc, char* argv[])
 {
+    std::map<char,int> yes;
+    std::string line;
+    unsigned count = 0;
+
+    while (std::getline(std::cin, line)) {
+        if (verbose)
+            std::cout << line << '\n';
+        if (line == std::string()) {
+            count += yes.size();
+            yes.clear();
+        } else {
+            for (unsigned i = 0; i < line.length(); i++)
+                ++yes[line[i]];
+        }
+    }
+
+    if (! yes.empty()) {
+        count += yes.size();
+        yes.clear();
+    }
+
+    std::cout << count << " categories were declared." << '\n';
+
     return 0;
 }
