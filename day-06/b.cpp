976a879d9df4284cf553db7b04d9604b787e49f9
Trevor Keller <trevor.keller@gmail.com>
2020-12-13 14:13:05 -0500

    boilerplat efor Day 6

---
+++ day-06/b.cpp
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include <iostream>
+#include <map>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
